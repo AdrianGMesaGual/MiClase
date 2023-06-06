40c843d69674a6b1415d537fb8269a17b263bd59
AdrianGMesaGual <adrian.mesagual@gmail.com>
2023-06-06 22:30:56 +0100

    Add two active

---
+++ test/miclase/MiClaseTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package miclase;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author adria
+ */
+public class MiClaseTest {
+    
+    public MiClaseTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class MiClase.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        MiClase.main(null);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
